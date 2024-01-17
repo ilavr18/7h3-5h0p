88d5815f4e99f64228d37d9c8e811b0e35c147de
Ruslan <91897771+ilavr18@users.noreply.github.com>
2024-01-17 16:34:55 +0300

    Edited tshirt changes func

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ const largeBtn = document.getElementById("large-btn")
 const whiteBtn = document.getElementById("white-btn")
 const yellowBtn = document.getElementById("yellow-btn")
 const greenBtn = document.getElementById("green-btn")
-const tshirtElement = document.getElementById("tshirt-image")
+const tshirtElement = [...document.getElementsByClassName("tshirt-image")]
 
 
 smallBtn.onclick = function () {
@@ -24,19 +24,19 @@ largeBtn.onclick = function () {
 }
 
 whiteBtn.onclick = function () {
-    tshirtElement.src = "img/tshirts/tshirt_white.jpg"
+    tshirtElement.forEach(element => element.src = "img/tshirts/tshirt_white.jpg")
     whiteBtn.style.border = "3px solid #cc0000"
     yellowBtn.style.border = "1px solid #000000"
     greenBtn.style.border = "1px solid #000000"
 }
 yellowBtn.onclick = function () {
-    tshirtElement.src = "img/tshirts/tshirt_yellow.jpg"
+    tshirtElement.forEach(element => element.src = "img/tshirts/tshirt_yellow.jpg")
     whiteBtn.style.border = "1px solid #000000"
     yellowBtn.style.border = "3px solid #cc0000"
     greenBtn.style.border = "1px solid #000000"
 }
 greenBtn.onclick = function () {
-    tshirtElement.src = "img/tshirts/tshirt_green.jpg"
+    tshirtElement.forEach(element => element.src = "img/tshirts/tshirt_green.jpg")
     whiteBtn.style.border = "1px solid #000000"
     yellowBtn.style.border = "1px solid #000000"
     greenBtn.style.border = "3px solid #cc0000"
